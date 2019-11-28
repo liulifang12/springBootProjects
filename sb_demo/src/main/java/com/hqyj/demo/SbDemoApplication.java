5c2a9ca3c22877e3b1ea84759d15f28641d367c7
YXJCKJ <llf1391444988@163.com>
2019-11-28 18:41:57 +0800

    "test"

--- sb_demo/src/main/java/com/hqyj/demo/SbDemoApplication.java
+++ sb_demo/src/main/java/com/hqyj/demo/SbDemoApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication springApplication = new SpringApplication(SbDemoApplication.class);
-		springApplication.run();
+		SpringApplication.run(SbDemoApplication.class, args);
 	}
 
 }
